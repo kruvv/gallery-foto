c314a8aaf8fa77e2a877439e0e44b380766fc209
kruvv <kruvv@mail.ru>
2024-12-19 15:03:36 +0300

    Edit setting

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 
 import "./style.css";
 import "@mdi/font/css/materialdesignicons.css";
-// import "vuetify/styles";
+import "../node_modules/vuetify/_styles.scss";
 import { createVuetify } from "vuetify";
 
 import * as components from "vuetify/components";
