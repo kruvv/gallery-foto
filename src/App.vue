dd14c629ed504c717f7e44ad894b9cefe5412fd9
kruvv <kruvv@mail.ru>
2024-12-19 11:11:04 +0300

    ...some

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,46 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <HelloWorld msg="Vite + Vue" />
+  <v-card class="mx-auto" color="grey-lighten-3" max-width="448">
+    <v-layout>
+      <v-app-bar color="teal-darken-4" image="https://picsum.photos/1920/1080?random">
+        <template v-slot:image>
+          <v-img gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"></v-img>
+        </template>
+
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        </template>
+
+        <v-app-bar-title>Title</v-app-bar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-app-bar>
+
+      <v-main>
+        <v-container fluid>
+          <v-row dense>
+            <v-col v-for="n in 4" :key="n" cols="12">
+              <v-card :subtitle="`Subtitle for Content ${n}`" :title="`Content ${n}`"
+                text="Lorem ipsum dolor sit amet consectetur, adipisicing elit.?"></v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-main>
+    </v-layout>
+  </v-card>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import vuetify from "vite-plugin-vuetify";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/gallery-foto",
-  plugins: [vue()],
+  base: "/gallery-foto/",
+  plugins: [vue(), vuetify()],
 });
